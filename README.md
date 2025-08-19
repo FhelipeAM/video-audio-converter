7a47bd6a9ab4635b6abc1b881db9bda52a9eb698
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2025-08-19 15:18:35 -0300

    Fixes to .gitignore
    - Fixes to .gitignore not properly excluding files in /converted/ nor /uploads/ and removal of an accidental file upload
    - Comments to further explain what each part of the code makes
    - Basic documentation
    - Cleanup on some parts of the code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# video-audio-converter
-A simple server that converts video to Mp3 audio and sends it back to the client
+# Video to MP3 Converter API
+A simple Node.js server that converts uploaded video files to MP3 audio. It provides both a RESTful API endpoint for programmatic access and a basic web UI for manual conversions.
+
+## Usage:
+
+Install [ffmpeg](https://ffmpeg.org/download.html), [Node.js](https://nodejs.org/en/download), [npm](https://www.npmjs.com/)
+
+Open the server folder in the terminal using 'cd server'
+
+Execute 'npm install' to get all dependencies 
+
+Run 'npm start' to initialize the server
+
+## Note:
+
+I've included a page in '/ui/' to show how a file should be uploaded via web. But you can also use curl to upload files.
+'curl -X POST -F "file=@**file_path**" http://**server_ip**:**port** -o **filename**.mp3'
--- server/main.js
+++ server/main.js
@@ -1,53 +1,61 @@
+//includes
 const express = require('express');
 const multer = require('multer');
 const { exec } = require('child_process');
 const path = require('path');
 
 const fs = require('fs');
-const fs2 = require('fs').promises;
 
 const app = express();
+
+//sets the folder to lay the client's (temporary) uploaded file
 const upload = multer({ dest: 'uploads/' });
 
-// const fs = FileSystem();
-app.use(express.static('converted'));
-
+//opens an endpoint on server root to receive an upload of a file
 app.post('/', upload.single('file'), async (req, res) => {
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
 
+    //get where the file got stored
     const filePath = req.file.path;
+    // using ffmpeg to convert the file into an mp3 (could be dynamically set via variable)
     const commandToExecute = `ffmpeg -i ${filePath} converted/${req.file.filename}.mp3`;
 
+    //runs the command
     exec(commandToExecute, async (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return res.status(500).send(`Error executing command: ${stderr}`);
         }
 
+        //deletes the temporary file
         fs.unlink(filePath, (err) => {
             if (err) console.error('Error deleting file:', err);
         });
-            
+        
+        //gets the converted file path
         const cFilePath = path.join(__dirname, 'converted', `${req.file.filename}.mp3`); 
         
-        let file = fs.readFile(cFilePath, 'binary', async (err, dat) => {
+        //reads the binary data stored in it
+        let file = fs.readFile(cFilePath, 'binary', (err, dat) => {
             if (err) {
                 console.log(err);
                 return;
             }
             
+            //sends it to the client
             res.setHeader('Content-Length', dat.length);
             res.setHeader('Content-Type', 'audio/mpeg');
             res.setHeader('Content-Disposition', `attachment; ${req.file.filename}.mp3`);
-            res.write(dat, 'binary', (bin2, bin3) => {});
+            res.write(dat, 'binary', () => {});
             res.end();
         });
     });
 
 });
 
+//opens the server port
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
